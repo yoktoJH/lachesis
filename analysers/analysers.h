256ebff193df5eb1d657261cba9e543e8a479496
Jozef Hoschek <xhoschek@fi.muni.cz>
2025-05-21 22:32:51 +0200

    delete moved files

--- analysers/analysers.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LLVMTOOL_LACHESIS_ANALYSERS__
-#define __LLVMTOOL_LACHESIS_ANALYSERS__
-
-void atomrace_init();
-void eraser_init();
-void eraser_terminate();
-void fasttrack_init();
-void fasttrack_terminate();
-#endif //__LLVMTOOL_LACHESIS_ANALYSERS__
